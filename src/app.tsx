fba4804da713ad1b1b8d5ba22b5eab9821c287f2
Everton Andrade <evertonfa7@gmail.com>
2021-09-18 12:38:39 -0300

    add styled components + theme + global styles + normalize css

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,13 @@
+import styled from 'styled-components'
+
 export function App() {
-	return <div>App</div>
+	return (
+		<div>
+			<Title>App</Title>
+		</div>
+	)
 }
+
+const Title = styled.h1`
+	color: ${(props) => props.theme.colors.primary}
+`
--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,27 @@
 import { App } from 'app'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import { theme } from 'resources/theme'
+import 'normalize.css'
 
 export function Root() {
-	return <App />
+	return (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>
+  )
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'DM Sans', sans-serif;
+  }
+`
