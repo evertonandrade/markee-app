f4707d64a6d7f4fc1622891947e154571457dabc
Everton Andrade <evertonfa7@gmail.com>
2021-09-20 23:25:45 -0300

    refatora componente de arquivo

---
+++ src/sidebar/file-item/file-item.tsx
@@ -0,0 +1,23 @@
+import { FileIcon, CloseIcon } from 'ui/icons'
+import * as S from './file-item-styles'
+
+type FileProps = {
+	name: string
+	active: boolean
+	status: 'editing' | 'saving' | 'saved'
+}
+
+export function FileItem({ name, active, status }: FileProps) {
+	return (
+		<S.ListItem active={active}>
+			<S.Link>
+				<FileIcon />
+				{name}
+			</S.Link>
+			{active && S.statusIcons[status]}
+			<S.CloseButton>
+				<CloseIcon />
+			</S.CloseButton>
+		</S.ListItem>
+	)
+}
