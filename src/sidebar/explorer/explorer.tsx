fe7cbe38e9e1ddfa028363e1df169205b68076b0
Everton Andrade <evertonfa7@gmail.com>
2021-09-23 18:23:19 -0300

    refatoração do componente explorer

--- src/sidebar/explorer/explorer.tsx
+++ src/sidebar/explorer/explorer.tsx
@@ -2,6 +2,7 @@ import * as S from './explorer-styles'
 import { FileItem } from "sidebar/file-item"
 import { File } from 'resources/types/files'
 
+
 type ExplorerProps = {
   files: File[]
 }
@@ -10,12 +11,10 @@ export function Explorer ({ files }: ExplorerProps) {
   return (
     <S.ContainerExplorer>
       <S.List>
-        {files.map(({ id, name, active, status }) => (
+        {files.map((file) => (
           <FileItem 
-            key={id} 
-            name={name}
-            active={active} 
-            status={status} 
+            key={file.id}
+            file={file}
           />
         ))}
       </S.List>
